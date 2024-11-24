2f1bb75007a5de763950fa5f21f3552f580569bd
PabloHernandez03 <pablohernandez6287@alumnos.udg.mx>
2024-11-23 20:47:43 -0600

    Improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link as ScrollLink } from "react-scroll";
 import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 
 function MyNavbar() {
   const [expanded, setExpanded] = useState(false);
@@ -9,7 +9,14 @@ function MyNavbar() {
 
   useEffect(() => {
     const navbarElement = document.querySelector(".navbar");
-    setNavbarHeight(navbarElement.offsetHeight);
+
+    const height = navbarElement.offsetHeight || 0;
+
+    if (window.innerWidth <= 768) {
+      setNavbarHeight(height * 4);
+    } else {
+      setNavbarHeight(height);
+    }
   }, []);
 
   const handleToggle = () => setExpanded(!expanded);
@@ -24,7 +31,7 @@ function MyNavbar() {
       expanded={expanded}
     >
       <Container>
-        <Navbar.Brand as={Link} to="/" smooth duration={300} onClick={handleClose}>
+        <Navbar.Brand as={Link} to="/" onClick={handleClose}>
           <img
             src={require("../img/logo.webp")}
             alt="Royal Style BarberStudio"
@@ -40,24 +47,52 @@ function MyNavbar() {
             <Nav.Link as={Link} to="/" smooth duration={300} offset={-navbarHeight} onClick={handleClose}>
               Inicio
             </Nav.Link>
-            <Nav.Link as={ScrollLink} to="barberos" smooth duration={300} offset={-navbarHeight} onClick={handleClose}>
+            <Nav.Link
+              as={ScrollLink}
+              to="barberos"
+              smooth={true}
+              duration={500}
+              offset={-navbarHeight}
+              onClick={handleClose}
+            >
               Barberos
             </Nav.Link>
-            <Nav.Link as={ScrollLink} to="servicios" smooth duration={300} offset={-navbarHeight} onClick={handleClose}>
+            <Nav.Link
+              as={ScrollLink}
+              to="servicios"
+              smooth={true}
+              duration={500}
+              offset={-navbarHeight}
+              onClick={handleClose}
+            >
               Servicios
             </Nav.Link>
-            <Nav.Link as={ScrollLink} to="horario" smooth duration={300} offset={-navbarHeight} onClick={handleClose}>
+            <Nav.Link
+              as={ScrollLink}
+              to="horario"
+              smooth={true}
+              duration={500}
+              offset={-navbarHeight}
+              onClick={handleClose}
+            >
               Horario
             </Nav.Link>
-            <Nav.Link as={ScrollLink} to="ubicacion" smooth duration={300} offset={-navbarHeight} onClick={handleClose}>
+            <Nav.Link
+              as={ScrollLink}
+              to="ubicacion"
+              smooth={true}
+              duration={500}
+              offset={-navbarHeight}
+              onClick={handleClose}
+            >
               Ubicación
             </Nav.Link>
             <Nav.Link
               className="btn btn-success text-white"
               as={ScrollLink}
               to="agenda"
-              smooth
-              duration={300}
+              smooth={true}
+              duration={500}
               offset={-navbarHeight}
               onClick={handleClose}
             >
